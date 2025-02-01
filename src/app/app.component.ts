2527d62266ed0d374f40e19e745ccaf5622a0799
andreas <andrewhoj@gmail.com>
2025-02-01 20:28:21 +0100

    Use backend prod endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent implements OnInit {
   title = 'frontend';
 
   async ngOnInit() {
-    const res = await fetch('http://localhost:5022/WeatherForecast')
+    const res = await fetch('https://image-gallery-backend.azurewebsites.net/WeatherForecast')
     const result = await res.json();
+    console.log(result)
   }
 }
